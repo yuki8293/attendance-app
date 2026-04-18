550ffdc29e80dc5493f554cf107dc5f33a4a2a43
yuki <raiku.yuki@icloud.com>
2026-04-18 10:55:42 +0900

    勤怠詳細画面のUIスタイル作成

--- src/resources/views/attendance/detail.blade.php
+++ src/resources/views/attendance/detail.blade.php
@@ -1,4 +1,4 @@
-<!-- 勤怠詳細画面　-->
+<!-- 勤怠詳細画面(一般ユーザー) -->
 
 @extends('layouts.app')
 
@@ -8,7 +8,7 @@
 
 @section('content')
 
-<h2>勤怠詳細</h2>
+<h2 class="detail-title">勤怠詳細</h2>
 
 <div class="page">
     <div class="detail-container">
@@ -21,27 +21,29 @@
             <input type="hidden" name="attendance_id" value="{{ $attendance->id }}">
 
             {{-- 名前（表示だけ） --}}
-            <div>
-                名前：{{ $attendance->user->name }}
+            <div class="row">
+                <div class="label">名前</div>
+                <div class="value">{{ $attendance->user->name }}</div>
             </div>
 
             {{-- 日付（表示だけ） --}}
-            <div>
-                日付：{{ \Carbon\Carbon::parse($attendance->work_date)->format('Y/m/d') }}
+            <div class="row">
+                <div class="label">日付</div>
+                <div class="value">
+                    {{ \Carbon\Carbon::parse($attendance->work_date)->format('Y/m/d') }}
+                </div>
             </div>
 
             {{-- 出勤・退勤 --}}
-            <div>
-                出勤・退勤：
-                <input
-                    type="time"
-                    name="start_time"
-                    value="{{ optional($attendance->start_time)->format('H:i') }}">
-                〜
-                <input
-                    type="time"
-                    name="end_time"
-                    value="{{ optional($attendance->end_time)->format('H:i') }}">
+            <div class="row">
+                <div class="label">出勤・退勤</div>
+                <div class="value">
+                    <input type="time" name="start_time"
+                        value="{{ optional($attendance->start_time)->format('H:i') }}">
+                    〜
+                    <input type="time" name="end_time"
+                        value="{{ optional($attendance->end_time)->format('H:i') }}">
+                </div>
             </div>
 
             {{-- 休憩（入力） --}}
@@ -49,26 +51,18 @@
             <div class="row">
                 <div class="label">休憩{{ $loop->iteration }}</div>
                 <div class="value">
-
-                    <input
-                        type="time"
-                        name="breaks[{{ $index }}][start]"
+                    <input type="time" name="breaks[{{ $index }}][start]"
                         value="{{ optional($break->start_time)->format('H:i') }}">
-
                     〜
-
-                    <input
-                        type="time"
-                        name="breaks[{{ $index }}][end]"
+                    <input type="time" name="breaks[{{ $index }}][end]"
                         value="{{ optional($break->end_time)->format('H:i') }}">
-
                 </div>
             </div>
             @endforeach
 
             {{-- 休憩（追加） --}}
             <div class="row">
-                <div class="label">休憩追加</div>
+                <div class="label">休憩2</div>
                 <div class="value">
                     <input type="time" name="break_new_start">
                     〜
@@ -77,16 +71,19 @@
             </div>
 
             {{-- 備考 --}}
-            <div>
-                備考：
-                <textarea name="note">{{ $attendance->note ?? '' }}</textarea>
+            <div class="row">
+                <div class="label">備考</div>
+                <div class="value">
+                    <textarea name="note">{{ $attendance->note ?? '' }}</textarea>
+                </div>
             </div>
-
-            {{-- 修正ボタン --}}
-            <button type="submit">修正</button>
-        </form>
-
     </div>
+
+    {{-- 修正ボタン --}}
+    <div class="detail-actions">
+        <button type="submit">修正</button>
+    </div>
+
 </div>
 
 @endsection
